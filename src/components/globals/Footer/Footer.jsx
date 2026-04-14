6150f9e03908107ae74f28e38c548417f39a0897
Alex <beachgirl.ak@gmail.com>
2026-04-14 00:00:31 -0400

    <update> Footer fix

--- src/components/globals/Footer/Footer.jsx
+++ src/components/globals/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import { useLenis } from "../../../context/LenisContext";
+import { NAV_ITEMS } from "../../../data/navItems";
+import Eyebrow from "../../Eyebrow/Eyebrow";
+
 // import { NAV_ITEMS } from "../../../constants/navItems";
 
-import './Footer.scss';
+import "./Footer.scss";
 
 function Footer({}) {
     const lenis = useLenis();
@@ -17,30 +21,44 @@ function Footer({}) {
 
     return (
         <footer>
-            <div className="footer-wrapper">
+            <div className="footer-upper">
                 <div className="footer-left">
-                    <h5 className="heading_xl">
+                    <Eyebrow variation="left" color="--gold-500" text="October 31st, 2026" />
+
+                    <h5 className="heading-xl">
                         Lets celebrate <span className="gold-italic">together</span>
                     </h5>
+
+                    <div className="footer-nav">
+                        {NAV_ITEMS.map((n, idx) => (
+                            <React.Fragment key={idx}>
+                                <Link
+                                    to={n.link || "/"}
+                                    className="footer-link"
+                                    onClick={(e) => {
+                                        if (!n.link && n.scrollId) {
+                                            e.preventDefault();
+                                            scrollToBlockById(n.scrollId);
+                                        }
+                                    }}
+                                >
+                                    <p>{n.text}</p>
+                                </Link>
+                            </React.Fragment>
+                        ))}
+                    </div>
                 </div>
-                <div className="footer-right">
-                    {/* {NAV_ITEMS.map((n, idx) => (
-                        <React.Fragment key={idx}>
-                            <Link
-                                to={n.link || "/"}
-                                className="footer-link"
-                                onClick={(e) => {
-                                    if (!n.link && n.scrollId) {
-                                        e.preventDefault();
-                                        scrollToBlockById(n.scrollId);
-                                    }
-                                }}
-                            >
-                                <p>{n.text}</p>
-                            </Link>
-                        </React.Fragment>
-                    ))} */}
-                </div>
+                <div className="footer-right"></div>
+            </div>
+
+            <div className="footer-lower">
+                <Link to={"/"} className="">
+                    <p className="">alexmaxwedding.com</p>
+                </Link>
+
+                <p>
+                    Designed & Developed by Alex
+                </p>
             </div>
         </footer>
     );
