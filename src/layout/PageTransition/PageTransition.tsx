4659e5c2aca0062491d8af56f1d0590d41548d8d
Alex <beachgirl.ak@gmail.com>
2026-05-19 19:22:07 -0400

    <update> Change transition animation

--- src/layout/PageTransition/PageTransition.tsx
+++ src/layout/PageTransition/PageTransition.tsx
@@ -15,23 +15,21 @@ const TransitionComponent = ({ children }: { children: React.ReactNode }) => {
             <Transition
                 key={location.pathname}
                 nodeRef={nodeRef}
-                timeout={500}
+                timeout={600}
                 onEnter={() => {
                     const node = nodeRef.current;
                     if (!node) return;
                     toggleCompleted(false);
-                    gsap.set(node, { autoAlpha: 0, scale: 0.8, xPercent: -100 });
+                    gsap.set(node, { xPercent: 100 });
                     gsap.timeline({ paused: true, onComplete: () => toggleCompleted(true) })
-                        .to(node, { autoAlpha: 1, xPercent: 0, duration: 0.25 })
-                        .to(node, { scale: 1, duration: 0.25 })
+                        .to(node, { xPercent: 0, duration: 0.5, ease: "power2.inOut" })
                         .play();
                 }}
                 onExit={() => {
                     const node = nodeRef.current;
                     if (!node) return;
                     gsap.timeline({ paused: true })
-                        .to(node, { scale: 0.8, duration: 0.2 })
-                        .to(node, { xPercent: 100, autoAlpha: 0, duration: 0.2 })
+                        .to(node, { xPercent: -100, duration: 0.5, ease: "power2.inOut" })
                         .play();
                 }}
             >
