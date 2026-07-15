48b479bb2fd916cb90b25cde7929e73b7fd7b0a0
Alex <beachgirl.ak@gmail.com>
2026-07-14 20:49:42 -0400

    Hide footer temp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
                         <main>{children}</main>
 
                         {/* <Footer /> */}
-                        <Footer2 />
+                        {/* <Footer2 /> */}
                     </LenisProvider>
                     <GlobalTooltip />
                 </TooltipProvider>
